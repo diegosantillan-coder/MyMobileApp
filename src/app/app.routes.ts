c9420c3b094336838793a11c7d718af7c9b6554a
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-08-09 18:56:45 -0500

    Inicializar el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,4 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
 ];
