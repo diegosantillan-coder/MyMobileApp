2a93ea5846a388be4a56b7dd2e30dfd14493dc83
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2024-08-12 09:16:57 -0500

    usar la camara en ionic

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButton, IonButtons, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButton, IonButtons, IonFab, IonFabButton, IonIcon, IonImg } from '@ionic/angular/standalone';
+import { PhotosService } from '../service/photos.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.page.html',
   styleUrls: ['./gallery.page.scss'],
   standalone: true,
-  imports: [IonIcon, IonFabButton, IonFab, IonButtons, IonButton, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonImg, IonIcon, IonFabButton, IonFab, IonButtons, IonButton, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class GalleryPage   {
 
-  constructor() { }
+  photos: string[] = [];
+  photoService = inject(PhotosService);
 
+  constructor(){
+    this.photos = this.photoService.photos;
+  }
+
+  async takePhoto() {
+    await this.photoService.addNewPhoto();
+  }
 }
